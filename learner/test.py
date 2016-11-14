02fdf09b5342392ad48a0919bac86e147505ea44
kenny <kjyoung@ualberta.ca>
2016-11-14 14:23:53 -0700

    minor changes to test

--- learner/test.py
+++ learner/test.py
@@ -3,11 +3,14 @@ import numpy as np
 
 learn = Learner()
 
-state = np.random.rand(6, 128, 128)
-action = learn.select_action(state)
-value = learn.evaluate_action(state, action)
-state_new = np.random.rand(6, 128, 128)
-reward = -1
-learn.update_memory(state, action, reward, state_new)
-learn.learn(1)
+for i in range(10):
+    state = np.random.rand(6, 128, 128)
+    action = learn.select_action(state)
+    print("action: "+str(action))
+    value = learn.evaluate_action(state, action)
+    state_new = np.random.rand(6, 128, 128)
+    reward = -1
+    learn.update_memory(state, action, reward, state_new)
+    print("memory size: "+str(learn.mem.size))
+    learn.learn(1)
 
