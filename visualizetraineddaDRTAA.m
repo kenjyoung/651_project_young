5821745fd7bbc370368c9c8eae6353f0fe445a02
kenny <kjyoung@ualberta.ca>
2016-11-24 21:00:28 -0700

    added code to visualize trained network, not yet functional

---
+++ visualizetraineddaDRTAA.m
@@ -0,0 +1,69 @@
+%% Visualize uLRTA* on problems
+% Oct 14, 2016
+% Vadim Bulitko
+
+close all
+clear
+clc
+diary off
+format short g
+
+if count(py.sys.path,'learner') == 0
+     insert(py.sys.path,int32(0),'learner');
+end
+
+import py.learner.Learner;
+learn = py.learner.Learner();
+
+%% Control parameters
+errorRate = 0;
+cutoff = 1000;
+
+da = 10;
+commit = 1;
+depth = 10;
+
+scenarioName = 'scenarios/yngvi_5.mat';
+
+fPos = [100 95 1280 720];
+fig = figure('Position',fPos);
+
+[~, sName, ~] = fileparts(scenarioName);
+
+diaryFileName = sprintf('logs/visualizeULRTA_%s.txt',sName);
+[~,~] = system(['rm ' diaryFileName]);
+diary(diaryFileName);
+
+fprintf('visualizeULRTA.m | ');
+disp(datetime);
+
+fprintf('Scenario %s\n',sName);
+fprintf('error STD %0.1f\n',errorRate);
+fprintf('cutoff %s\n\n',hrNumber(cutoff));
+
+rng('shuffle');
+
+tttTotal = tic;
+
+diary off
+diary on
+
+%% Load the scenario
+loadedScenario = load(scenarioName);
+numMaps = length(loadedScenario.maps);
+numProblems = length(loadedScenario.problem);
+numProblemsPerMap = numProblems/numMaps;
+problemI = randi(numProblems);
+fprintf('Loaded %s\n\tmaps %d | problems %d, %0.1f per map\n',scenarioName,numMaps,numProblems,numProblemsPerMap);
+fprintf('\nRunning problem %d\n',problemI);
+
+%% Run the algorithm
+tt = tic;
+
+[subopt,~,solved] = traineddaDRTAAEvalVis(loadedScenario.problem,loadedScenario.maps,problemI,cutoff,errorRate,da,depth,commit);
+
+fprintf('\tsubopt %0.1f | solved %d | %s\n',subopt,solved,sec2str(toc(tt)));
+
+%% Wrap up
+fprintf('\nTotal time %s\n',sec2str(toc(tttTotal)));
+diary off
